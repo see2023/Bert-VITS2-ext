46b658f8b404d3f15445da9f0a3252bf90091635
see2023 <wavetestingood@gmail.com>
2023-12-27 11:11:20 +0800

    Bert-VITS2 2.3 original version

---
+++ README.md
@@ -0,0 +1,42 @@
+<div align="center">
+
+<img alt="LOGO" src="https://cdn.jsdelivr.net/gh/fishaudio/fish-diffusion@main/images/logo_512x512.png" width="256" height="256" />
+
+# Bert-VITS2
+
+VITS2 Backbone with multilingual bert
+
+For quick guide, please refer to `webui_preprocess.py`.
+
+简易教程请参见 `webui_preprocess.py`。
+
+## 请注意，本项目核心思路来源于[anyvoiceai/MassTTS](https://github.com/anyvoiceai/MassTTS) 一个非常好的tts项目
+## MassTTS的演示demo为[ai版峰哥锐评峰哥本人,并找回了在金三角失落的腰子](https://www.bilibili.com/video/BV1w24y1c7z9)
+
+[//]: # (## 本项目与[PlayVoice/vits_chinese]&#40;https://github.com/PlayVoice/vits_chinese&#41; 没有任何关系)
+
+[//]: # ()
+[//]: # (本仓库来源于之前朋友分享了ai峰哥的视频，本人被其中的效果惊艳，在自己尝试MassTTS以后发现fs在音质方面与vits有一定差距，并且training的pipeline比vits更复杂，因此按照其思路将bert)
+
+## 成熟的旅行者/开拓者/舰长/博士/sensei/猎魔人/喵喵露/V应当参阅代码自己学习如何训练。
+
+### 严禁将此项目用于一切违反《中华人民共和国宪法》，《中华人民共和国刑法》，《中华人民共和国治安管理处罚法》和《中华人民共和国民法典》之用途。
+### 严禁用于任何政治相关用途。
+#### Video:https://www.bilibili.com/video/BV1hp4y1K78E
+#### Demo:https://www.bilibili.com/video/BV1TF411k78w
+#### QQ Group：815818430
+## References
++ [anyvoiceai/MassTTS](https://github.com/anyvoiceai/MassTTS)
++ [jaywalnut310/vits](https://github.com/jaywalnut310/vits)
++ [p0p4k/vits2_pytorch](https://github.com/p0p4k/vits2_pytorch)
++ [svc-develop-team/so-vits-svc](https://github.com/svc-develop-team/so-vits-svc)
++ [PaddlePaddle/PaddleSpeech](https://github.com/PaddlePaddle/PaddleSpeech)
++ [emotional-vits](https://github.com/innnky/emotional-vits)
++ [fish-speech](https://github.com/fishaudio/fish-speech)
++ [Bert-VITS2-UI](https://github.com/jiangyuxiaoxiao/Bert-VITS2-UI)
+## 感谢所有贡献者作出的努力
+<a href="https://github.com/fishaudio/Bert-VITS2/graphs/contributors" target="_blank">
+  <img src="https://contrib.rocks/image?repo=fishaudio/Bert-VITS2"/>
+</a>
+
+[//]: # (# 本项目所有代码引用均已写明，bert部分代码思路来源于[AI峰哥]&#40;https://www.bilibili.com/video/BV1w24y1c7z9&#41;，与[vits_chinese]&#40;https://github.com/PlayVoice/vits_chinese&#41;无任何关系。欢迎各位查阅代码。同时，我们也对该开发者的[碰瓷，乃至开盒开发者的行为]&#40;https://www.bilibili.com/read/cv27101514/&#41;表示强烈谴责。)
---
+++ preprocess_text.py
@@ -0,0 +1,141 @@
+import json
+from collections import defaultdict
+from random import shuffle
+from typing import Optional
+import os
+
+from tqdm import tqdm
+import click
+from text.cleaner import clean_text
+from config import config
+from infer import latest_version
+
+preprocess_text_config = config.preprocess_text_config
+
+
+@click.command()
+@click.option(
+    "--transcription-path",
+    default=preprocess_text_config.transcription_path,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+)
+@click.option("--cleaned-path", default=preprocess_text_config.cleaned_path)
+@click.option("--train-path", default=preprocess_text_config.train_path)
+@click.option("--val-path", default=preprocess_text_config.val_path)
+@click.option(
+    "--config-path",
+    default=preprocess_text_config.config_path,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+)
+@click.option("--val-per-lang", default=preprocess_text_config.val_per_lang)
+@click.option("--max-val-total", default=preprocess_text_config.max_val_total)
+@click.option("--clean/--no-clean", default=preprocess_text_config.clean)
+@click.option("-y", "--yml_config")
+def preprocess(
+    transcription_path: str,
+    cleaned_path: Optional[str],
+    train_path: str,
+    val_path: str,
+    config_path: str,
+    val_per_lang: int,
+    max_val_total: int,
+    clean: bool,
+    yml_config: str,  # 这个不要删
+):
+    if cleaned_path == "" or cleaned_path is None:
+        cleaned_path = transcription_path + ".cleaned"
+
+    if clean:
+        with open(cleaned_path, "w", encoding="utf-8") as out_file:
+            with open(transcription_path, "r", encoding="utf-8") as trans_file:
+                lines = trans_file.readlines()
+                # print(lines, ' ', len(lines))
+                if len(lines) != 0:
+                    for line in tqdm(lines):
+                        try:
+                            utt, spk, language, text = line.strip().split("|")
+                            norm_text, phones, tones, word2ph = clean_text(
+                                text, language
+                            )
+                            out_file.write(
+                                "{}|{}|{}|{}|{}|{}|{}\n".format(
+                                    utt,
+                                    spk,
+                                    language,
+                                    norm_text,
+                                    " ".join(phones),
+                                    " ".join([str(i) for i in tones]),
+                                    " ".join([str(i) for i in word2ph]),
+                                )
+                            )
+                        except Exception as e:
+                            print(line)
+                            print(f"生成训练集和验证集时发生错误！, 详细信息:\n{e}")
+
+    transcription_path = cleaned_path
+    spk_utt_map = defaultdict(list)
+    spk_id_map = {}
+    current_sid = 0
+
+    with open(transcription_path, "r", encoding="utf-8") as f:
+        audioPaths = set()
+        countSame = 0
+        countNotFound = 0
+        for line in f.readlines():
+            utt, spk, language, text, phones, tones, word2ph = line.strip().split("|")
+            if utt in audioPaths:
+                # 过滤数据集错误：相同的音频匹配多个文本，导致后续bert出问题
+                print(f"重复音频文本：{line}")
+                countSame += 1
+                continue
+            if not os.path.isfile(utt):
+                # 过滤数据集错误：不存在对应音频
+                print(f"没有找到对应的音频：{utt}")
+                countNotFound += 1
+                continue
+            audioPaths.add(utt)
+            spk_utt_map[language].append(line)
+            if spk not in spk_id_map.keys():
+                spk_id_map[spk] = current_sid
+                current_sid += 1
+        print(f"总重复音频数：{countSame}，总未找到的音频数:{countNotFound}")
+
+    train_list = []
+    val_list = []
+
+    for spk, utts in spk_utt_map.items():
+        shuffle(utts)
+        val_list += utts[:val_per_lang]
+        train_list += utts[val_per_lang:]
+
+    shuffle(val_list)
+    if len(val_list) > max_val_total:
+        train_list += val_list[max_val_total:]
+        val_list = val_list[:max_val_total]
+
+    with open(train_path, "w", encoding="utf-8") as f:
+        for line in train_list:
+            f.write(line)
+
+    with open(val_path, "w", encoding="utf-8") as f:
+        for line in val_list:
+            f.write(line)
+
+    json_config = json.load(open(config_path, encoding="utf-8"))
+    json_config["data"]["spk2id"] = spk_id_map
+    json_config["data"]["n_speakers"] = len(spk_id_map)
+    # 新增写入：写入训练版本、数据集路径
+    json_config["version"] = latest_version
+    json_config["data"]["training_files"] = os.path.normpath(train_path).replace(
+        "\\", "/"
+    )
+    json_config["data"]["validation_files"] = os.path.normpath(val_path).replace(
+        "\\", "/"
+    )
+    with open(config_path, "w", encoding="utf-8") as f:
+        json.dump(json_config, f, indent=2, ensure_ascii=False)
+    print("训练集和验证集生成完成！")
+
+
+if __name__ == "__main__":
+    preprocess()
